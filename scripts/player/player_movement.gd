f1b5f65532095a30d8cf9c75471a66c935e622d6
Mohitsaroya <mohitsaroya999@gmail.com>
2026-05-01 00:56:53 -0600

    added player movement script and test level to test out the movement.

---
+++ scripts/player/player_movement.gd
@@ -0,0 +1,19 @@
+extends CharacterBody2D
+
+@export var move_speed := 200.0
+
+func _ready():
+	print("PLAYER SCRIPT READY")
+
+func _physics_process(_delta):
+	var input_direction := Vector2.ZERO
+	
+	input_direction.x = Input.get_action_strength("move_right") - Input.get_action_strength("move_left")
+	input_direction.y = Input.get_action_strength("move_down") - Input.get_action_strength("move_up")
+	
+	input_direction = input_direction.normalized()
+	velocity = input_direction * move_speed
+	
+	print("input:", input_direction, " velocity:", velocity, " position:", position)
+	
+	move_and_slide()
