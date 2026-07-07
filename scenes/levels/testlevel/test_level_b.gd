7a5e203e228067615d737db28742b09978731cf0
jnthnwhiting <jnthnwhiting@gmail.com>
2026-07-06 19:16:05 -0600

    comments

--- scenes/levels/testlevel/test_level_b.gd
+++ scenes/levels/testlevel/test_level_b.gd
@@ -2,6 +2,7 @@ extends Node2D
 var player: Player
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	#the same player from the previous scene is loaded
 	if scene_manager.player:
 		player = scene_manager.player
 		add_child(player)
